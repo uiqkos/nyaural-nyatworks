0c99eb22a691e0587a3de1322f7f8bc977bbbba1
uiqkos <uiqkos@gmail.com>
2021-12-13 18:09:09 +0300

    (web) чел на коте натурально мискликает

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,15 @@
 let alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+let keyboardLayoutMap = '1234567890-=qwertyuiop[] asdfghjkl;\'\\ zxcvbnm,./ '
+
+function missclick(ch) {
+  let i = keyboardLayoutMap.indexOf(ch)
+  let vertical = (Math.floor(Math.random() * 3) - 1) * 12
+  let horizontal = (Math.floor(Math.random() * 3) - 1)
+  let rch = keyboardLayoutMap[Math.min(Math.max(0, vertical + horizontal + i), keyboardLayoutMap.length - 1)]
+  return rch === ' ' ? ch : rch;
+}
+
 function nextInt() {
   return (Math.random() * (400 - 100) + 100)
 }
@@ -26,7 +36,10 @@ export async function typingText(text_ids, textSources, cursor_id, completeClass
 
     for (let j = 0; j < source.length; j++) {
       if (Math.random() > 0.85 && j != source.length - 1) {
-        let randomch = alphabet[Math.floor(Math.random() * (alphabet.length - 1))]
+        let randomch =  missclick(source[j])
+        while (source[j] === randomch) {
+          randomch = missclick(source[j])/*alphabet[Math.floor(Math.random() * (alphabet.length - 1))]*/
+        }
         text.innerHTML += randomch
         await new Promise(r => setTimeout(r, nextInt()))
         text.innerHTML = text.innerHTML.slice(0, text.innerHTML.length - 1)
