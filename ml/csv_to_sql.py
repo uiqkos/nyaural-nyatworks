005011af0d6147a060b2b70db54b006e037bb694
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-02-07 12:06:25 +0300

    чистка после flask

--- ml/csv_to_sql.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlite3 import connect
-
-con = connect('data/data')
-
-# train = pd.read_csv('./data/train.csv', index_col='id', header=0, error_bad_lines=False, engine="python")
-test = pd.read_csv('toxic/data/test.csv', index_col='id', header=0, error_bad_lines=False, engine="python")
-
--- ml/sentiment/__init__.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras import Model
-
-
-def load_model() -> Model:
-    pass
