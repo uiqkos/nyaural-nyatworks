40099f89a8087ab185b9e815f2cf145e259a6ac1
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-03-02 13:54:23 +0300

    (utils) get_item_or

--- nya_utils/functools.py
+++ nya_utils/functools.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from operator import itemgetter
 
 
 def identity(x): return x
@@ -10,3 +11,11 @@ def _compose2(f1, f2):
 
 def compose(*fs):
     return reduce(_compose2, fs)
+
+
+def get_item_or(obj, item, default=None, getter=None, astype=None):
+    getter = getter or obj.__getitem__
+    astype = astype or (type(default) if default else identity)
+    if item in obj:
+        return astype(getter(item))
+    return astype(default)
