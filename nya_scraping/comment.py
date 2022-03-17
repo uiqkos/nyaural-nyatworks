9a72bedd9d0e24d8d194d0b6141341a60c8e40ff
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-03-17 12:53:50 +0300

    исправлен баг с __getattr__ в Comment

--- nya_scraping/comment.py
+++ nya_scraping/comment.py
@@ -1,7 +1,4 @@
-from copy import copy
-from dataclasses import field, dataclass, asdict
-from functools import partial
-from typing import List, Callable, Any
+from dataclasses import dataclass, asdict
 
 
 @dataclass(unsafe_hash=True)
@@ -18,38 +15,17 @@ class Comment:
     date: str = None
     id: str = None
 
-    comments: List['Comment'] = field(default_factory=list)
-
-    @classmethod
-    @property
-    def empty(cls):
-        return cls('')
-
     @property
     def attributes(self):
-        return ['id', 'text', 'author', 'date', 'comments']
+        return ['id', 'text', 'author', 'date']
 
-    def to_dict(self, comments=True):
-        d = {
+    def to_dict(self):
+        return {
             'id': self.id,
             'text': self.text,
             'author': asdict(self.author) if self.author else None,
             'date': self.date
         }
-
-        if comments:
-            return {
-                **d, 'comments': [comment.to_dict() for comment in self.comments]
-            }
-
-        return d
-
-    def __len__(self):
-        return 1 + sum(map(len, self.comments))
-    #
-    # @property
-    # def id(self):
-    #     return hash(self.extra)
 
 
 @dataclass
@@ -67,26 +43,17 @@ class CommentDecorator(Comment):
     def attributes(self):
         return self._comment.attributes
 
-    def __getattr__(self, item):
-        if item in self._comment.attributes:
-            return getattr(self._comment, item)
-        return self.__dict__[item]
+    def __getattribute__(self, item):
+        comment = object.__getattribute__(self, '_comment')
+        if item in comment.attributes:
+            return getattr(comment, item)
+        return object.__getattribute__(self, item)
 
-    def to_dict(self, comments=True):
+    def to_dict(self):
         return {
             **{
                 attr: getattr(self, attr)
                 for attr in self.attributes
-                if attr != 'comments'
             },
-            **self._comment.to_dict(comments)
+            **self._comment.to_dict()
         }
-
-
-def map_comment(func: Callable[[Comment], Comment], comment):
-    comments = copy(comment.comments)
-
-    comment = func(comment)
-    comment.comments = list(map(partial(map_comment, func), comments))
-
-    return comment
